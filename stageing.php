9278db47e6e94908cc5d8ebe57a2a6bd9a472067
unknown <rianaddy@gmail.com>
2024-04-01 16:00:46 -0400

    first commit

---
+++ stageing.php
@@ -0,0 +1,17 @@
+<?php
+    $output;
+    $i = 0;
+
+    $ids1 = array();
+    $ids2 = array();
+
+    $names1 = array();
+    $names2 = array();
+
+    while (i < 7) {
+        $output = sql here;
+        if $output1 {
+            array_push($ids1, $output);
+        }
+    }
+?>
