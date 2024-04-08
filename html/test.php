c748315a0ab491fd5da5893eb649f1bc4921814b
unknown <rianaddy@gmail.com>
2024-04-07 23:18:30 -0400

    trying sql round2

--- html/test.php
+++ html/test.php
@@ -15,6 +15,7 @@ $conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
+    echo "well Shit";
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
