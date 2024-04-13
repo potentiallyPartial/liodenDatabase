be193bcf42e1d2b63e44eb025bbd2dc33ef784d1
unknown <rianaddy@gmail.com>
2024-04-13 15:43:12 -0400

    adding case funcnality

--- html/test.php
+++ html/test.php
@@ -4,7 +4,37 @@
 <body>
 
 <?php
-include 'functions.php';
+function NCL($bseID, $bseName){
+    echo "$bseName is a NCL they can be found in explore\n";
+    echo "Dev: the Id for the base is $bseID\n";
+}
+
+function Custom($bseID, $bseName){
+    echo "Your base: $bseName is a custom base.\n";
+    echo "Dev: the Id for the base is $bseID\n";
+}
+
+function BreedOnly($bseID, $bseName){
+    echo "Your base: $bseName is a BreedOnly base.\n";
+    echo "Dev: the Id for the base is $bseID\n";
+}
+
+function Combo($bseID, $bseName){
+    echo "Your base: $bseName is a Combo base.\n";
+    echo "Dev: the Id for the base is $bseID\n";
+}
+
+function Applicator($bseID, $bseName){
+    echo "Your base: $bseName is a Applicator base.\n";
+    echo "Dev: the Id for the base is $bseID\n";
+}
+
+function Error(){
+    echo "There is a un caught statment in the switch statment";
+}
+?>
+
+<?php
 // make conection with my sql server
 $conn = new mysqli("localhost","Tiamat","5he@ds","lioden");
 
@@ -34,7 +64,7 @@ $stmt->bind_param("s", $usrInput);
  $stmt->fetch();
 
  // switch for handling diffrent base Types
-/*
+
 switch ($usrInput){
   case Custom:
     Custom($bseId, $usrInput);
