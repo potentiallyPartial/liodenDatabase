6914fa08ccbb152d6e5b463072f21f190652026a
unknown <rianaddy@gmail.com>
2024-04-29 22:45:21 -0400

    cstom discription update

--- html/output.php
+++ html/output.php
@@ -59,7 +59,7 @@ error_reporting(E_ALL);
 
 
 if ($bseType == "Custom"){
-  $output = "Your base: $usrInput is a custom base. <br> Dev: the Id for the base is $bseId";
+  $output = "Your base: $usrInput is a custom base. It can be applied via Oasis"
 }
 
 elseif ($bseType == "Breed Only"){
